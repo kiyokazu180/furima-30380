0d279eb9ebf7fa95fa159f496d8c598ba88ef7b9
kiyokazu180 <kiyo77761296@gmail.com>
2021-01-10 16:39:30 +0900

    バリデーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, :name_lc, :name_gc, :name_l, :name_g, :birth, presence: true
+  validates :name_lc, :name_gc, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
+  validates :name_l, :name_g, format: { with: /\A[ァ-ヶー－]+\z/ }
 end
