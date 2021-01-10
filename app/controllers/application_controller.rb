e35da80bd904c5db9f84fa0e31d04e2c4910e3aa
kiyokazu180 <kiyo77761296@gmail.com>
2021-01-10 11:25:55 +0900

    ログイン、ログアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  # before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  private
+
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :name_lc, :name_gc, :name_l, :name_g, :birth])
+  end  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, :name_lc, :name_gc, :name_l, :name_g, :birth, presence: true
 end
