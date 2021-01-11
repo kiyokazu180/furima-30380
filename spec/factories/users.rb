aa7004688fc00da645ff8d2f819c5d20b021d956
kiyokazu180 <kiyo77761296@gmail.com>
2021-01-11 10:57:43 +0900

    テストコード初期設定

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname                 {"puyo"}
+    email                    {"kkk@gmail.com"}
+    password                 {"aaaaaa1"}
+    password_confirmation    {"aaaaaa1"}
+    name_lc                  {"岡本"}
+    name_gc                  {"次郎"}
+    name_l                   {"オカモト"}
+    name_g                   {"ジロウ"}
+    
+  end
+end    
