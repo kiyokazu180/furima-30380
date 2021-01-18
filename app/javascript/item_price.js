fb8649b5eddd825a86d8fde1edf92ace122f2083
kiyokazu180 <kiyo77761296@gmail.com>
2021-01-18 10:26:13 +0900

    値段の表示

---
+++ app/javascript/item_price.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', put)
+function put(){
+  const put_price = document.getElementById("item-price")
+  const put_tax = document.getElementById("add-tax-price")
+  const put_profit = document.getElementById("profit")
+  put_price.addEventListener('input', function(){
+    const price = document.getElementById("item-price").value;
+    console.log(price);
+    put_tax.innerHTML = price*0.1;
+    put_profit.innerHTML= price*0.9;
+  })
+}
+  
