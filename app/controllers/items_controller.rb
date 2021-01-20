f84d894dff0d4c594a65dfe2155601ebd28dd88b
kiyokazu180 <kiyo77761296@gmail.com>
2021-01-20 10:21:50 +0900

    詳細の表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,8 +12,17 @@ class ItemsController < ApplicationController
   end
   
   def edit
-
+    @item = Item.find(params[:id])
   end  
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(strong_method)
+       redirect_to edit_item_path(@item.id)
+    else
+       render :edit
+    end
+  end
 
   def show
     @item = Item.find(params[:id])
