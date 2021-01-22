9ca7ac2703f31c9887275916ffd1f49db138b33b
kiyokazu180 <kiyo77761296@gmail.com>
2021-01-22 15:06:21 +0900

    購入フォームの設定

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,5 +1,22 @@
 class AddressesController < ApplicationController
   def new
+    @item = Item.find(params[:id])
+    @address = Address.new
+  end  
 
+  def create
+    @address = Address.new(address_params)
+    if @address.valid?
+      @address.save
+      return redirect_to root_path
+    else
+      render :index
+    end  
+  end
+  
+  private
+    
+  def address_params
+    params.require(:address).permit(:postal_code, :region_id, :area, :building, :city, :phone_number)
   end  
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,12 @@
 class Address < ApplicationRecord
+  belongs_to :region
+  with_options numericality: { other_than: 1 } do
+    validates :region_id
+  end
+  with_options presence: true do
+    validates :postal_code
+    validates :city
+    validates :area
+    validates :phone_number
+  end  
 end
